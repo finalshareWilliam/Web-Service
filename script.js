dfec898be870cab1b26d3b464821d2eefa7261b9
finalshareWilliam <william.h.souza@hotmail.com>
2022-05-08 19:34:18 -0300

    v.1.0
    v.1

---
+++ script.js
@@ -0,0 +1,141 @@
+// API openweathermap
+const api = {
+    key: "64ed82577ced7f69cb1687f0ce536131", // key para o API
+    base: "https://api.openweathermap.org/data/2.5/", // local da requisição
+    lang: "pt_br", // definindo a linguaguem para API
+    units: "metric" // definindo a unidade para API
+}
+
+// linkando os elementos do API para as classes.
+const city = document.querySelector('.cidade')
+const date = document.querySelector('.data');
+const container_img = document.querySelector('.container-img');
+const container_temp = document.querySelector('.container-temp');
+const temp_number = document.querySelector('.container-temp div');
+const temp_unit = document.querySelector('.container-temp span');
+const weather_t = document.querySelector('.tempo');
+const search_input = document.querySelector('.form-control');
+const search_button = document.querySelector('.btn');
+const low_high = document.querySelector('.min-max');
+
+// geolocalização
+window.addEventListener('load', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setPosition, showError);
+    }
+    else {
+        alert('navegador não suporta geolozalicação');
+    }
+    function setPosition(position) {
+        console.log(position)
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        coordResults(lat, long);
+    }
+    function showError(error) {
+        alert(`erro: ${error.message}`);
+    }
+})
+
+function coordResults(lat, long) {
+    fetch(`${api.base}weather?lat=${lat}&lon=${long}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`http error: status ${response.status}`)
+            }
+            return response.json();
+        })
+        .catch(error => {
+            alert(error.message)
+        })
+        .then(response => {
+            displayResults(response)
+        });
+}
+
+// INPUT por click.
+search_button.addEventListener('click', function() {
+    searchResults(search_input.value)
+})
+
+// INPUT por teclar ENTER.
+search_input.addEventListener('keypress', enter)
+function enter(event) {
+    key = event.keyCode
+    if (key === 13) {
+        searchResults(search_input.value)
+    }
+}
+
+// requisão na API pelo metodo fetch.
+function searchResults(city) {
+    fetch(`${api.base}weather?q=${city}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`http error: status ${response.status}`)
+            }
+            return response.json();
+        })
+        .catch(error => {
+            alert(error.message)
+        })
+        .then(response => {
+            displayResults(response)
+        });
+}
+
+// função de criação do estado metereologico daquela cidade a partir dos dados requisitados da API.
+function displayResults(weather) {
+    console.log(weather)
+
+    city.innerText = `${weather.name}, ${weather.sys.country}`; // requisição para o API nos dando a cidade e pais.
+
+    let now = new Date();
+    date.innerText = dateBuilder(now);                          // requisição para o API nos dando a data e aqui tambem chamamos a função dateBuilder para criação da mesma.
+
+    let iconName = weather.weather[0].icon;
+    container_img.innerHTML = `<img src="./icons/${iconName}.png">`;    // aqui obtemos da API o codigo de meterologia e linkamos como nosso icones.
+
+    let temperature = `${Math.round(weather.main.temp)}`                // aqui obtemos da API a temperatura da cidade.
+    temp_number.innerHTML = temperature;
+    temp_unit.innerHTML = `°c`;
+
+    weather_tempo = weather.weather[0].description;
+    weather_t.innerText = capitalizeFirstLetter(weather_tempo)
+
+    low_high.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`; // aqui obtemos da API o min e max da cidade.
+}
+
+// função de criação de data a partir dos dados requisitados da API.
+function dateBuilder(d) {
+    let days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julio", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+    let day = days[d.getDay()]; //dia 0-6
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date} ${month} ${year}`;
+}
+
+// função para mudar o sistema metrico de temperatura de celsius para fahrenheit.
+container_temp.addEventListener('click', changeTemp)
+function changeTemp() {
+    temp_number_now = temp_number.innerHTML
+
+    if (temp_unit.innerHTML === "°c") {
+        let f = (temp_number_now * 1.8) + 32
+        temp_unit.innerHTML = "°f"
+        temp_number.innerHTML = Math.round(f)
+    }
+    else {
+        let c = (temp_number_now - 32) / 1.8
+        temp_unit.innerHTML = "°c"
+        temp_number.innerHTML = Math.round(c)
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
